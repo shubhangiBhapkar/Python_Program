5d357f0b97c547fef17749e667c86414754952b7
web Development <shubhangibhapkar35@gmail.com>
2025-11-19 15:36:55 +0530

    Operators

---
+++ Basic/Operators.py
@@ -0,0 +1,19 @@
+x = 10
+y=5
+# Airthmatic operators
+print("Addition ",x+y)
+print("Subtraction ",x-y)
+print("Multiplication ",x*y)
+print("Division ",x/y)
+print("Modulus ",x%y)
+
+# Comparioson
+print(x>y)
+print(x==y)
+print(x<y)
+print(y<x)
+
+print("Logical Operators")
+print(x>y and x==y)
+print(x<y or x==y)
+print(not x==y)
