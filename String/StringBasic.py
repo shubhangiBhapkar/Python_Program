fd6b386639bbf342356ef34f874193440b0ec1b3
web Development <shubhangibhapkar35@gmail.com>
2025-11-22 21:48:43 +0530

    string basic

---
+++ String/StringBasic.py
@@ -0,0 +1,18 @@
+str1 = 'priya'
+str2 = "shubhii"
+str3 = '''shubham'''
+
+print(str1)
+print(str2)
+print(str3)
+
+# Concatenation 
+print(str1 +" "+ str2)
+
+# Indexing
+print("Length of string  : ", len(str1))
+print("letter at position str1[0] : ", str1[0])
+
+# Immutable
+# str1[0] = 'R'
+# print(str1)
