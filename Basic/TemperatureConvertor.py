50355da3f2384ea0d432cadf513dbfa31ca072e3
web Development <shubhangibhapkar35@gmail.com>
2025-11-22 21:11:11 +0530

    Temperature Convertor

---
+++ Basic/TemperatureConvertor.py
@@ -0,0 +1,9 @@
+#Accpt the temperature in celsius convert it to fahrenheit
+
+celsius = float(input("Enter temperature in celsius : "))
+
+fahrenheit = (celsius * (9/5) ) + 32
+print("Fahrenheit : ",fahrenheit)
+
+kelvin = celsius + 273.15
+print("Kelvin : ",kelvin)
