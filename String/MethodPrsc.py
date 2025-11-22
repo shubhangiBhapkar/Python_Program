254a4f33e0c98654d2f3998eaa62f97493103646
web Development <shubhangibhapkar35@gmail.com>
2025-11-22 22:49:05 +0530

    string methods

---
+++ String/MethodPrsc.py
@@ -0,0 +1,10 @@
+str = "Shubhangi is very sensitive person"
+
+# convert to lower case
+print(str.lower())
+
+# replace all white space with _
+print(str.replace(" ","_"))
+
+# print
+print(str)
---
+++ String/stringMethos.py
@@ -0,0 +1,8 @@
+str = "shubhangi bhapkar"
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.find('sh'))
+print(str.replace("bhapkar","ambani"))
+print(str.count("a"))
+
