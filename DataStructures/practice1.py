29691cb332bb31e72a721338126f5e58de3a8f6c
web Development <shubhangibhapkar35@gmail.com>
2025-11-23 23:14:38 +0530

    dict practice

---
+++ DataStructures/practice1.py
@@ -0,0 +1,10 @@
+# create a dictionary name marks of 3 subjects ,
+# add the subject one by one and print the final dictionary
+
+marks = {}
+
+print(type(marks))
+marks["engilsh"] = 95
+marks["math"] = 89
+marks["maathi"]= 91
+print(marks)
