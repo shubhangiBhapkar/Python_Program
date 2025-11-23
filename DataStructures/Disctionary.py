108116ad326eeecc642dbf67a353a7b4fc9fd070
web Development <shubhangibhapkar35@gmail.com>
2025-11-23 23:09:55 +0530

    dictionary

---
+++ DataStructures/Disctionary.py
@@ -0,0 +1,15 @@
+student = {
+    "name" : "shubhangi",
+    "age" : 21,
+    "city" : "Ahilyanagar",
+    "marks" : 91,
+    5 : "abc"
+}
+print(type(student))
+print(student)
+
+
+student["name"] ="priya" 
+print(student)
+print(student.keys())
+print(student.items())
